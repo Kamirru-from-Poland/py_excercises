6c4612ed9060e8cfb3563f36738c2fa95e37e96a
Kamirru <->
2023-11-28 22:26:36 +0100

    2nd done

--- chapter1/main.py
+++ chapter1/main.py
@@ -1,2 +1,8 @@
 def mins_in_week():
     return 7 * 24 * 60
+
+# introducer = lambda: None (= pass for lambdas)
+
+introducer = lambda: """I am Kamirru.
+I am 22 years old.
+I like coding."""
---
+++ chapter1/test_introducer.py
@@ -0,0 +1,5 @@
+from main import *
+def test_introducer():
+    assert introducer() == """I am Kamirru.
+I am 22 years old.
+I like coding."""
